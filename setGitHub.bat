3c74886e81f7d087410a847923376f38f448488c
Java488-oss <job.mail1358@gmail.com>
2022-08-11 20:28:48 +0600

    Создание проекта 0

--- setGitHub.bat
+++ setGitHub.bat
@@ -6,6 +6,6 @@ git add .
 set /p "id=Input commit: "
 git commit -m "%id%"
 git branch -M master
-git remote add origin https://Java488-oss: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@github.com/Java488-oss/LessonEntityManager.git
+git remote set-url origin https://Java488-oss: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@github.com/Java488-oss/LessonEntityManager.git
 git push origin master
 pause
